5e5d01d9af82ebea057c80d68532ab02a978ece7
Gaëtan RENAUDEAU <renaudeau.gaetan@gmail.com>
2011-03-19 23:27:04 +0100

    Continued refactor..

--- javascripts/main_refactor.js
+++ javascripts/main_refactor.js
@@ -14,12 +14,16 @@ $(function(){
     // Colors constants
     R: 1, G: 2, RG: 3, B: 4, RB: 5, GB: 6, RGB: 7,
     
+    defaults: {
+      value: this.R
+    },
+    
     initialize: function() {
       var color = this.get('color');
       if(color) {
         var value = 0;
         var self = this;
-        _.each(color.toUpperCase().split(''), function(a){
+        _.each(color.toUpperCase(), function(a){
           if(self[a]) value |= self[a];
         });
         this.set({ value: value });
@@ -75,6 +79,10 @@ $(function(){
   var Orientation = lta.Orientation = Backbone.Model.extend({
     TOPLEFT: 0, TOP: 1, TOPRIGHT: 2, RIGHT: 3, BOTTOMRIGHT: 4, BOTTOM: 5, BOTTOMLEFT: 6, LEFT: 7,
     
+    defaults: {
+      value: this.RIGHT
+    },
+    
     initialize: function(){
       var orientation = this.get('orientation');
       if(orientation) {
@@ -102,8 +110,6 @@ $(function(){
       return (this.get('value')-3) * Math.PI / 4;
     }
   });
-  
-  
   
   var Position = lta.Position = Backbone.Model.extend({
     initialize: function(){
@@ -144,28 +150,8 @@ $(function(){
   
   
   
-  // A game object displayable on the Map
-  var MapObject = lta.MapObject = Backbone.Model.extend({
-    
-    defaults: {
-      kind: 'none' // receptor, laser, wall, bomb, tool
-    },
-    
-    ray: function() {
-      
-    }
-    
-    // Some methods used by map view
-  });
   
-  
-  var Tool = lta.Tool = MapObject.extend({
-    
-    defaults: {
-      kind: 'tool',
-      type: 'SIMPLE'
-    },
-    
+  var ToolType = lta.ToolType = Backbone.Model.extend({
     initialize: function() {
       var type = this.get('type');
       var icon = new Image();
@@ -178,85 +164,86 @@ $(function(){
       for(var o in rayTransforms)
         this.input[new Orientation({ orientation: o }).val()] = rayTransforms[o];
     },
-    
     ray: function(orientation, color) {
       var f = this.input[orientation.val()];
       if(typeof(f)==='undefined') return {};
       return f(color);
     }
-    
-  });
+  })
   
   var ToolCollection = lta.ToolCollection = Backbone.Collection.extend({
-    model: Tool
+    model: ToolType,
+    findByType: function(type) {
+      return this.find(function(el){ return el.get('type') === type.toUpperCase() });
+    }
   });
   
-  var Tools = lta.Tools = new ToolCollection([
-    new Tool({
-      type: 'SIMPLE',
-      rayTransforms: {
-        topleft: function(color) { return { bottomleft: color } },
-            top: function(color) { return { bottom: color } },
-         bottom: function(color) { return { top: color } },
-     bottomleft: function(color) { return { topleft: color } },
-           left: function(color) { return { left: color } }
-      }
-    }),
-    new Tool({
-      type: 'DOUBLE',
-      rayTransforms: {
-        topleft: function(color) { return { bottomleft: color } },
-            top: function(color) { return { bottom: color } },
-       topright: function(color) { return { bottomright: color } },
-          right: function(color) { return { right: color } },
-    bottomright: function(color) { return { topright: color } },
-         bottom: function(color) { return { top: color } },
-     bottomleft: function(color) { return { topleft: color } },
-           left: function(color) { return { left: color } }
-      }
-    }),
-    new Tool({
-      type: 'CONE',
-      rayTransforms: {
-            top: function(color) { return { left: color } },
-         bottom: function(color) { return { left: color } },
-           left: function(color) { return { top: color, bottom: color } }
-      }
-    }),
-    new Tool({
-      type: 'QUAD',
-      rayTransforms: {
-          right: function(color) { return { top: color, left: color } },
-           left: function(color) { return { top: color, right: color } }
-      }
-    }),
-    new Tool({
-      type: 'REFRACTOR',
-      rayTransforms: {
-        topleft: function(color) { return { bottomright: color } },
-          right: function(color) { return { topleft: color } },
-    bottomright: function(color) { return { topleft: color } },
-           left: function(color) { return { bottomright: color } }
-      }
-    }),
-    new Tool({
-      type: 'TRIANGLE',
-      rayTransforms: {
-        topleft: function(color) { return { right: color } },
-        right: function(color) { return { topleft: color, bottomleft: color } },
-        bottomleft: function(color) { return { right: color } }
-      }
-    }),
-    new Tool({
-      type: 'SPLITER',
-      rayTransforms: {
-        left: function(color) { return {
-          topright: color.filter(new Color('R')),
-          right: color.filter(new Color('G')),
-          bottomright: color.filter(new Color('B')) } }
-      }
-    })
-  ]);
+  // A game object displayable on the Map
+  var MapObject = lta.MapObject = Backbone.Model.extend({
+    
+    defaults: {
+      kind: 'none', // receptor, laser, wall, bomb, tool
+      orientation: new Orientation,
+      position: new Position
+    },
+    
+    position: function(val) {
+      if(typeof(val)==='undefined')
+        return this.get('position');
+      this.set({ position: val });
+      return this;
+    },
+    
+    orientation: function(val) {
+      if(typeof(val)==='undefined')
+        return this.get('orientation');
+      this.set({ orientation: val });
+      return this;
+    },
+    
+    turnLeft: function() {
+      orientation( orientation().next() );
+    },
+    
+    turnRight: function() {
+      orientation( orientation().prev() );
+    },
+    
+    ray: function() {
+      
+    }
+  });
+  
+  var Bomb = lta.Bomb = MapObject.extend({
+    
+  })
+  
+  var Laser = lta.Laser = MapObject.extend({
+    
+  })
+  
+  var Receptor = lta.Receptor = MapObject.extend({
+    defaults: {
+      color: new Color
+    }
+  })
+  
+  var Tool = lta.Tool = MapObject.extend({
+    
+    defaults: {
+      kind: 'tool',
+      type: null
+    },
+    
+    initialize: function() {
+      this.type = this.get('type');
+    },
+    
+    ray: function() {
+      return this.type.apply(this.type, arguments);
+    }
+    
+  });
   
   
   var Sound = lta.Sound = Backbone.Model.extend({
@@ -269,29 +256,27 @@ $(function(){
     }
   });
   
-  var Case = lta.Case = Backbone.Model.extend({
-    
-  });
-  
-  var Grid = lta.Grid = Backbone.Model.extend({
-    
-  });
-  
-  var Panel = lta.Panel = Backbone.Model.extend({
-    
+  // The grid object
+  var Map = lta.Map = Backbone.Collection.extend({
+    model: MapObject,
+    initialize: function(){
+      
+    }
   });
   
   var Level = lta.Level = Backbone.Model.extend({
-    
+    initialize: function(){
+      
+    }
   });
+  
+  var LevelCollection = lta.LevelCollection = Backbone.Collection.extend({
+    model: Level
+  })
+  
   
   // Manage the laser canvas
   var RayTracer = lta.RayTracer = Backbone.View.extend({
-    
-  });
-  
-  // Manage the bottom panel with tools
-  var PanelView = lta.PanelView = Backbone.View.extend({
     
   });
   
@@ -299,6 +284,129 @@ $(function(){
   var GridView = lta.GridView = Backbone.View.extend({
     
   });
+  
+  
+  var PanelTool = lta.PanelTool = Backbone.Model.extend({
+    defaults: {
+      number: 0,
+      tooltype: 'simple'
+    },
+    number: function(val) {
+      if(typeof(val)==='undefined') return this.get('number');
+      this.set({ number: val });
+      return this;
+    }
+  });
+  
+  var Panel = lta.Panel = Backbone.Collection.extend({
+    model: PanelTool
+  })
+  
+  // Manage the bottom panel with tools
+  var PanelView = lta.PanelView = Backbone.View.extend({
+    
+  });
+  
+  /// Datas
+  var Tools = new ToolCollection([
+    new ToolType({
+      type: 'SIMPLE',
+      rayTransforms: {
+        topleft: function(color) { return { bottomleft: color } },
+            top: function(color) { return { bottom: color } },
+         bottom: function(color) { return { top: color } },
+     bottomleft: function(color) { return { topleft: color } },
+           left: function(color) { return { left: color } }
+      }
+    }),
+    new ToolType({
+      type: 'DOUBLE',
+      rayTransforms: {
+        topleft: function(color) { return { bottomleft: color } },
+            top: function(color) { return { bottom: color } },
+       topright: function(color) { return { bottomright: color } },
+          right: function(color) { return { right: color } },
+    bottomright: function(color) { return { topright: color } },
+         bottom: function(color) { return { top: color } },
+     bottomleft: function(color) { return { topleft: color } },
+           left: function(color) { return { left: color } }
+      }
+    }),
+    new ToolType({
+      type: 'CONE',
+      rayTransforms: {
+            top: function(color) { return { left: color } },
+         bottom: function(color) { return { left: color } },
+           left: function(color) { return { top: color, bottom: color } }
+      }
+    }),
+    new ToolType({
+      type: 'QUAD',
+      rayTransforms: {
+          right: function(color) { return { top: color, left: color } },
+           left: function(color) { return { top: color, right: color } }
+      }
+    }),
+    new ToolType({
+      type: 'REFRACTOR',
+      rayTransforms: {
+        topleft: function(color) { return { bottomright: color } },
+          right: function(color) { return { topleft: color } },
+    bottomright: function(color) { return { topleft: color } },
+           left: function(color) { return { bottomright: color } }
+      }
+    }),
+    new ToolType({
+      type: 'TRIANGLE',
+      rayTransforms: {
+        topleft: function(color) { return { right: color } },
+        right: function(color) { return { topleft: color, bottomleft: color } },
+        bottomleft: function(color) { return { right: color } }
+      }
+    }),
+    new ToolType({
+      type: 'SPLITER',
+      rayTransforms: {
+        left: function(color) { return {
+          topright: color.filter(new Color('R')),
+          right: color.filter(new Color('G')),
+          bottomright: color.filter(new Color('B')) } }
+      }
+    })
+  ]);
+  
+  var Levels = new LevelCollection([
+    {
+      name: "Level 1 - Getting started",
+      description: "Mirror is the key.",
+      
+      grid: [
+        [0, 4, Laser, 'r', 'right'],
+        [6, 9, Receptor, 'r']
+      ],
+      
+      tools: [
+        { type: 'SIMPLE', number: 1 }
+      ]
+    },
+    
+    {
+      name: "Level 2 - Spliting rays",
+      description: "One source. Three receptors.",
+      
+      grid: [
+        [4, 0, Laser, 'G', 'BOTTOM'],
+        [9, 6, Receptor, 'G'],
+        [6, 9, Receptor, 'G'],
+        [0, 6, Receptor, 'G']
+      ],
+      
+      tools: [
+        { type: 'CONE', number: 1 },
+        { type: 'QUAD', number: 1 }
+      ]
+    }
+  ])
   
   // Manage the game
   var Game = lta.Game = Backbone.Controller.extend({
